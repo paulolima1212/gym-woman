c225996418fa698ca23e3a7caec08d52f17615a4
paulolima1212 <plima12121984@gmail.com>
2023-04-17 11:50:27 -0300

    feat: create pages galery and plans

---
+++ src/pages/galery/index.tsx
@@ -0,0 +1,7 @@
+export function Galery() {
+  return (
+    <div>
+      <h1>Galeria</h1>
+    </div>
+  )
+}
---
+++ src/pages/plans/index.tsx
@@ -0,0 +1,7 @@
+export function Plans() {
+  return (
+    <div>
+      <h1>Planos</h1>
+    </div>
+  )
+}
